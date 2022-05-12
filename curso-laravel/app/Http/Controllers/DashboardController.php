7382e81975b8c441b2614e2fa8c6981927372e60
alejopanto <jose.pantoja@testu.co>
2022-05-11 20:36:18 -0500

    create migration and model

--- curso-laravel/app/Http/Controllers/DashboardController.php
+++ curso-laravel/app/Http/Controllers/DashboardController.php
@@ -6,9 +6,12 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index(){
-        return view('test', [
-            'title' => 'Hello'
-        ]);
+    public function index(Request $request)
+    {
+        // dd($request->query('title'));
+        $title = $request->query('title');
+
+
+        return view('test', compact('title'));
     }
 }
