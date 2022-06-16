f2e98d1d129f0062e43b408ee3302726d8e397ec
alejopanto <jose.pantoja@testu.co>
2022-06-15 20:51:35 -0500

    show blog

--- Introduction laravel/introduction/routes/web.php
+++ Introduction laravel/introduction/routes/web.php
@@ -11,13 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'PageController@posts');
+Route::get('blog/{post}', 'PageController@post')->name('post');
 
 Auth::routes();
 
