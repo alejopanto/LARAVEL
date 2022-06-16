8cfe13bdd72d305becbbea8ad1688db861a1fc72
alejopanto <jose.pantoja@testu.co>
2022-06-15 22:47:45 -0500

    show img and iframe

--- Introduction laravel/introduction/resources/views/post.blade.php
+++ Introduction laravel/introduction/resources/views/post.blade.php
@@ -6,6 +6,19 @@
         <div class="col-md-8">
             <div class="card mb-4">
                 <div class="card-body">
+
+                    @if ($post->image)
+                        <h3>Imagen</h3>
+                        <img src="{{ $post->get_image }}" class="card-img-top">
+                    @endif
+                    <hr>
+                    @if($post->iframe)
+                        <h3>Contenido Embebido</h3>
+                        <div class="embed-responsive embed-responsive-16by9">
+                            {!! $post->iframe !!}
+                        </div>
+                    @endif
+
                     <h5 class="card-title"> {{ $post->title }} </h5>
                     <p class="card-test"> 
                         {{ $post->body }} 
--- Introduction laravel/introduction/resources/views/posts.blade.php
+++ Introduction laravel/introduction/resources/views/posts.blade.php
@@ -7,6 +7,19 @@
             @foreach ($posts as $post)
                 <div class="card mb-4">
                     <div class="card-body">
+
+                        @if ($post->image)
+                            <h3>Imagen</h3>
+                            <img src="{{ $post->get_image }}" class="card-img-top">
+                        @endif
+                        <hr>
+                        @if($post->iframe)
+                            <h3>Contenido Embebido</h3>
+                            <div class="embed-responsive embed-responsive-16by9">
+                                {!! $post->iframe !!}
+                            </div>
+                        @endif
+
                         <h5 class="card-title"> {{ $post->title }} </h5>
                         <p class="card-test"> 
                             {{ $post->get_excert }} 
